dcba77b490b5c9e73644bd2889bd0034044dd7a6
Neon <neon@saahild.com>
2025-10-17 22:47:28 -0400

    update: idk anymore

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 require("dotenv/config");
 const express = require("express");
-const openpgp = require("openpgp");
 const { PrismaClient } = require("@prisma/client");
-const { createSchema } = require("./schema");
+const { envSchema } = require("./schema");
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+require("./api")(app, prisma);
+
+
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+
 app.get("/note/:id", async (req, res) => {
   const id = req.params.id;
   if (!id) {
@@ -24,49 +29,9 @@ app.get("/note/:id", async (req, res) => {
   res.render(`file`, { data });
 });
 
-app.post("/api/create", async (req, res) => {
-  const body = req.body;
-  let properData;
-  try {
-    properData = createSchema.parse(body);
-  } catch (e) {
-    return res.status(422).json({ error: e.message });
-  }
-  try {
-    const birth = await prisma.file.create({
-      data: {
-        encContent: properData.content,
-        pubUserId: properData.pubKey,
-        title: properData.title,
-      },
-    });
-
-    res.status(201).json({
-      message: "OK CREATED",
-    });
-  } catch (e) {
-    return res.status(500).json({ message: e.message });
-  }
-});
 
 app.get("/create", (req, res) => {
   res.render("create");
-});
-
-app.get("/api/genkey", (req, res) => {
-  const email = req.query.email;
-  const name =
-    req.query.name ||
-    Math.random().toFixed(20).toString().split(".")[1].slice(0, 7);
-  openpgp
-    .generateKey({
-      userIDs: [{ name, email }],
-      curve: "ed25519",
-      passphrase: req.query.passphrase,
-    })
-    .then((d) => {
-      res.json(d);
-    });
 });
 
 app.listen(process.env.PORT || 3000, () => {
--- src/schema.js
+++ src/schema.js
@@ -18,3 +18,4 @@ module.exports = {
   createSchema,
   envSchema,
 };
+
